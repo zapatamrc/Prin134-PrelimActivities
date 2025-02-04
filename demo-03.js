c4f145348fddb450497786097246fb3933ad1d37
Marc Zapata <mrcnthnyzpt@gmail.com>
2025-02-04 13:01:44 +0800

    num3

--- demo-03.js
+++ demo-03.js
@@ -10,10 +10,15 @@ function separateNum(numbers) {
         }
     }
 
-    console.log("Numbers:",numbersArray)
-    console.log("Even Numbers:", evenNumbers, "| Count:", evenNumbers.length);
-    console.log("Odd Numbers:", oddNumbers, "| Count:", oddNumbers.length);
+    return { evenNumbers, oddNumbers }; 
 }
 
 let numbersArray = [12, 7, 5, 20, 33, 42, 19, 8, 10];
-separateNum(numbersArray);
+let { evenNumbers, oddNumbers } = separateNum(numbersArray); 
+
+let even = `Even Numbers: ${evenNumbers.join(', ')} | Count: ${evenNumbers.length}`;
+let odd = `Odd Numbers: ${oddNumbers.join(', ')} | Count: ${oddNumbers.length}`;
+
+console.log("Numbers:", numbersArray.join(', '));
+console.log(even);
+console.log(odd);
